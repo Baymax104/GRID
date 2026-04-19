f0c4bee5add42fa1405f75be9921770e8f960507
John Wei <wzy1048168235@163.com>
2026-04-19 18:31:20 +0800

    build(main): 修改运行配置

--- rkmeans_inference_flat.sh
+++ rkmeans_inference_flat.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+NPROC_PER_NODE=2
+
 conda run -n GRID --no-capture-output \
-  torchrun --nproc_per_node=2 -m src.inference \
+  OMP_NUM_THREADS=$(( $(nproc) / NPROC_PER_NODE )) \
+  torchrun --nproc_per_node=$NPROC_PER_NODE -m src.inference \
   experiment=rkmeans_inference_flat \
-  embedding_path=logs/inference/runs/2026-03-12/14-34-34/pickle/merged_predictions_tensor.pt \
-  ckpt_path=logs/train/runs/2026-03-22/08-46-47/checkpoints/checkpoint_000_000300.ckpt \
+  embedding_path=logs/sem_embeds_inference_flat/runs/2026-03-22/23-38-29/pickle/merged_predictions_tensor.pt \
+  ckpt_path=logs/rkmeans_train_flat/runs/2026-03-22/23-52-08/checkpoints/checkpoint_000_000300.ckpt \
   data_dir=data/beauty \
   embedding_dim=768 \
   num_hierarchies=3 \
--- rkmeans_train_flat.sh
+++ rkmeans_train_flat.sh
@@ -5,9 +5,9 @@ NPROC_PER_NODE=2
 conda run -n GRID --no-capture-output \
   OMP_NUM_THREADS=$(( $(nproc) / NPROC_PER_NODE )) \
   torchrun --nproc_per_node=$NPROC_PER_NODE -m src.train \
-  should_skip_retry=true \
+  +should_skip_retry=true \
   experiment=rkmeans_train_flat \
-  embedding_path=logs/inference/runs/2026-03-12/14-34-34/pickle/merged_predictions_tensor.pt \
+  embedding_path=logs/sem_embeds_inference_flat/runs/2026-03-22/23-38-29/pickle/merged_predictions_tensor.pt \
   data_dir=data/beauty \
   embedding_dim=768 \
   num_hierarchies=3 \
